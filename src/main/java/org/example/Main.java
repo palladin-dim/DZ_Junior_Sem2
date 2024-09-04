52705071d4d55fc7405a28a215b5c42d7f164a27
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2024-09-04 18:29:29 +0600

    DZ_Sem2.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.lang.reflect.Method;
+
+
+/**
+ * Используя Reflection API, напишите программу, которая выводит на экран все методы класса String.
+ */
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        stringClassMethods();
+    }
+
+    static void stringClassMethods() {
+
+        Class<String> clazz = String.class;
+        Method[] methods = clazz.getDeclaredMethods();
+
+        int num = 0;
+        for (Method method : methods) {
+            num++;
+            System.out.println("Метод № " + num + ": " + method.getName());
+        }
     }
 }
